79b7d1400afedc866844c82834cb466c504e9327
Filip Vrba <filipvrba@pm.me>
2023-05-01 21:34:52 +0200

    change import

--- lib/bef-1.0.0/bef.rb
+++ lib/bef-1.0.0/bef.rb
@@ -1,4 +1,6 @@
-require 'lib/bef-1.0.0/bef/constants.rb'
-
 module Bef
+  VERSION = '1.0.0'
+  ROOT = "modules/dr-bef-rb/lib/bef-#{VERSION}/bef"
+  
+  require "#{Bef::ROOT}/constants.rb"
 end
--- lib/bef-1.0.0/bef/constants.rb
+++
@@ -1,5 +0,0 @@
-module Bef
-  module Constants
-    VERSION = '1.0.0'
-  end
-end
